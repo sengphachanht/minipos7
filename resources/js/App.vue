2d0d902969305aa8b6f30883ee6d00dd5deada60
unknown <SENGPHACHANH@ML-SENGPHACHANH.mitrphol.local>
2023-02-15 16:50:38 +0700

    upload to github v2

---
+++ resources/js/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1> VueJS Framwork </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos7App',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    components:{
+
+    },
+    computed:{
+
+    },
+
+    methods: {
+        
+    },
+    wacth(){
+
+    },
+    create(){
+
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
